6f7b0032903d0ade2157f88ed3f11a7ee5fbcefd
Chris P <sahib@online.de>
2015-04-05 17:00:16 +0200

    Weichen gestellt; Tornado + Flask + Python3

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='calk',
+    version='0.0',
+    description='A stupid web mpd client',
+    url='http://github.com/studentkittens/calk',
+    author='Cat & Moose',
+    author_email='sahib@online.de',
+    license='GPLv3',
+    packages=['calk'],
+    zip_safe=False
+)
